4b70f78f1eb7a6e9f6882c3080b7a84ef33d1c14
Mike Kilmade <mkilmade@nycap.rr.com>
2018-09-20 16:10:53 -0400

    Formatted and added phpDoc

--- getTrend.php
+++ getTrend.php
@@ -1,19 +1,26 @@
 <?php
+/* 
+ * @package Horse Information System (HIS)
+ * @author Mike Kilmade <mkilmade.nycap.rr.com>
+ * @version 0,001
+ * 
+ * @name getTrend
+ * @parmam string $_GET['trend'] requested trend name
+ * /
+
+ /* set up common environment needed to process all trend code */
 session_start();
-require_once('includes/config.inc.php');
-require_once('includes/connection.inc.php');;
-
-require_once('includes/trends/keyRaces.inc.php');
-require_once('includes/trends/multipleWins.inc.php');
-require_once('includes/trends/previouslyRanAtMeet.inc.php');
-require_once('includes/trends/previousRaceAtMeetPerCard.inc.php');
-require_once('includes/trends/classTally.inc.php');
-require_once('includes/trends/previousTrackWins.inc.php');
-require_once('includes/trends/previousFinishTally.inc.php');
-
-// call trend function requested
+require_once ('includes/config.inc.php');
+require_once ('includes/connection.inc.php');
 $conn = new Connection();
-$_GET['trend']($conn);
+$trendName = $_GET['trend'];
+
+/* call trend function requested */
+require_once ("includes/trends/$trendName.inc.php");
+$trendName($conn);
+
+/* clean up environment */
 $conn->close();
+$trendName=null;
 
 ?>
