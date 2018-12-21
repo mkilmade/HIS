24d9c2fbaa1013363cdeeb9049da2d6b30dbb660
Mike Kilmade <mkilmade@nycap.rr.com>
2018-12-21 14:38:44 -0500

    Formatting

--- update_defaults.php
+++ update_defaults.php
@@ -15,10 +15,9 @@ if ($_SERVER ['REQUEST_METHOD'] == 'POST') {
 		if ($value == $current->$field) {
 			unset ( $post [$field] );
 			continue;
-		} else {
-			if ($field == 'race_meet_id')
-				// don't use track conditions for newly selected race meet
-				unset ( $_SESSION ['dirt_track_condition'] );
+		} else if ($field == 'race_meet_id') {
+			// don't use track conditions for newly selected race meet
+			unset ( $_SESSION ['dirt_track_condition'] );
 			unset ( $_SESSION ['turf_track_condition'] );
 		} // race_meet_id check 'if'
 	} // value check 'else'
